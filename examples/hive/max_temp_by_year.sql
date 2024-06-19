65001c0a39a513d39c354f88d5e83a6f8850dcf4
ensean <liyxzsu@gmail.com>
2024-06-19 23:01:13 +0800

    Update max_temp_by_year.sql

--- examples/hive/max_temp_by_year.sql
+++ examples/hive/max_temp_by_year.sql
@@ -1,4 +1,8 @@
+SET hive.cli.print.header=true;
+SET hive.query.name=ExtremeWeather;
 
+SET hive.tez.input.format=org.apache.hadoop.hive.ql.io.CombineHiveInputFormat;
+SET mapreduce.input.fileinputformat.split.maxsize=16777216;
 
 create table max_temp_by_year as
 
